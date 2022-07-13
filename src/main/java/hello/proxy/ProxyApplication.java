0b9ead47d5678c501f7090186d62b5a3fa6ab36f
김형익 <kiomnd2@naver.com>
2022-07-14 00:08:28 +0900

    프록시 패턴적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,16 +1,26 @@
 package hello.proxy;
 
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.interface_proxy.InterfaceProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+//@Import(AppV1Config.class)
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProxyApplication.class, args);
+    }
+
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
     }
 
 }
