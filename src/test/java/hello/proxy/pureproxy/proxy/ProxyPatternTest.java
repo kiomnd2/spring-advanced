6131f5779d46966c2e3876550e0d5a2cbfce6515
김형익 <kiomnd2@naver.com>
2023-01-07 23:28:12 +0900

    예제코드 제거

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,9 +1,5 @@
 package hello.proxy.pureproxy.proxy;
 
-import hello.proxy.pureproxy.decorator.code.Component;
-import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
-import hello.proxy.pureproxy.decorator.code.MessageDecorator;
-import hello.proxy.pureproxy.decorator.code.RealComponent;
 import hello.proxy.pureproxy.proxy.code.*;
 import org.junit.jupiter.api.Test;
 
@@ -29,22 +25,4 @@ public class ProxyPatternTest {
         client.execute();
     }
 
-    @Test
-    void decorator1() {
-        Component realComponent = new RealComponent();
-        Component messageDecorator = new MessageDecorator(realComponent);
-        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
-        client.execute();
-    }
-
-    @Test
-    void decorator2() {
-        Component realComponent = new RealComponent();
-        Component messageDecorator = new MessageDecorator(realComponent);
-
-        Component timeDecorator = new TimeDecorator(messageDecorator);
-
-        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
-        client.execute();
-    }
 }
