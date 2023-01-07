259cd9d14c43683d21751d189cb0c1b8e5d69608
김형익 <kiomnd2@naver.com>
2023-01-08 00:11:32 +0900

    데코레이터 패터

---
+++ src/test/java/hello/proxy/pureproxy/decorator/code/DecoratorPatternClient.java
@@ -0,0 +1,20 @@
+package hello.proxy.pureproxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DecoratorPatternClient {
+
+    public Component component;
+
+
+    public DecoratorPatternClient(Component component) {
+        this.component = component;
+    }
+
+    public void execute() {
+        String result = this.component.operation();
+        log.info("result={}", result);
+    }
+
+}
