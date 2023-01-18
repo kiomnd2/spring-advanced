3431392e2abc6d1e3a062d7def93a10b273a20fc
김형익 <kiomnd2@naver.com>
2023-01-18 20:54:25 +0900

    Proxy Factory 정의

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -5,14 +5,23 @@ import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.ConcreteProxyConfig;
 import hello.proxy.config.interface_proxy.InterfaceProxyConfig;
+import hello.proxy.config.proxyfactory.ProxyFactoryConfigV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
+//@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+@Import(ProxyFactoryConfigV1.class)
+//@Import(ProxyFactoryConfigV2.class)
+//@Import(BeanPostProcessorConfig.class)
+//@Import(AutoProxyConfig.class)
+//@Import(AopConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
